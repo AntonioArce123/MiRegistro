f7cca06a36d9b84db0954c0fd97ecd3733bc7cc8
AntonioArce123 <71289815+AntonioArce123@users.noreply.github.com>
2024-04-09 18:58:20 +0000

    09/04/24

--- Scripts/2024/03/12/ejemplo.sql
+++ Scripts/2024/03/12/ejemplo.sql
@@ -66,7 +66,33 @@ select * from Products
     WHERE (ProductName <> 'Pavlova')
         or  (CategoryID = 2 and SupplierID=3)
         or (productid in (3,5, 7))
-        
 
 
+--09/04/24--       
+select * from Products (ProductName, SupplierID, CategoryID, UnitPrice)
+insert into Products (ProductName, SupplierID, CategoryID, UnitPrice)
+values (
+    'Queso monita', 14, 4, 3, 11
+    )
+    delete from Products 
+        where ProductID = '78'
 
+
+UPDATE Products
+set UnitPrice = UnitPrice * 1.1
+where SupplierID = 12 
+
+select * from products
+
+select * from SupplierID
+    where SupplierID = 12
+
+select ProductName, CompanyName 
+    from Products, Suppliers
+
+select ProductID, ProductName, UnitPrice, CompanyName from Products  
+    inner join Suppliers on Products.SupplierID = SupplierID.SupplierID
+    where UnitPrice BETWEEN 15 and 30
+    --order by CompanyName
+    --order by UnitInStock
+    order by Suppliers.Country
